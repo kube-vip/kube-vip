eedcc583c159f6443757ef222a393855df9571ee
Dan Finneran <daniel.finneran@gmail.com>
2020-02-06 17:01:54 +0000

    Load-Balancer is bound to vip
    This set of changes allow the load balancer to move with the virtual IP and to be cleanly shutdown as a node is removed.

--- pkg/loadbalancer/manager.go
+++ pkg/loadbalancer/manager.go
@@ -1,14 +1,71 @@
 package loadbalancer
 
-import "github.com/thebsdbox/kube-vip/pkg/kubevip"
+import (
+	"fmt"
 
-//lbState - manages the state of load balancer instances
-type lbState []struct {
-	stop       chan bool             // Asks LB to stop
-	stopped    chan bool             // LB is stopped
-	lbInstance *kubevip.LoadBalancer // pointer to a LB instance
+	log "github.com/sirupsen/logrus"
+	"github.com/thebsdbox/kube-vip/pkg/kubevip"
+)
+
+//LBInstance - manages the state of load balancer instances
+type LBInstance struct {
+	stop     chan bool             // Asks LB to stop
+	stopped  chan bool             // LB is stopped
+	instance *kubevip.LoadBalancer // pointer to a LB instance
 }
 
-func ParseLBS() error {
+//LBManager - will manage a number of load blancer instances
+type LBManager struct {
+	loadBalancer []LBInstance
+}
+
+//Add - handles the building of the load balancers
+func (lm *LBManager) Add(bindAddress string, lb *kubevip.LoadBalancer) error {
+	newLB := LBInstance{
+		stop:     make(chan bool, 1),
+		stopped:  make(chan bool, 1),
+		instance: lb,
+	}
+
+	switch lb.Type {
+	case "tcp":
+		err := newLB.startTCP(bindAddress)
+		if err != nil {
+			return err
+		}
+	case "http":
+		err := newLB.startHTTP(bindAddress)
+		if err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("Unknown Load Balancer type [%s]", lb.Type)
+	}
+
+	lm.loadBalancer = append(lm.loadBalancer, newLB)
+	return nil
+}
+
+//StopAll - handles the building of the load balancers
+func (lm *LBManager) StopAll() error {
+	log.Debugf("Stopping [%d] loadbalancer instances", len(lm.loadBalancer))
+	for x := range lm.loadBalancer {
+		err := lm.loadBalancer[x].Stop()
+		if err != nil {
+			return err
+		}
+	}
+	// Reset the loadbalancer entries
+	lm.loadBalancer = nil
+	return nil
+}
+
+//Stop - handles the building of the load balancers
+func (l *LBInstance) Stop() error {
+
+	close(l.stop)
+
+	<-l.stopped
+	log.Infof("Load Balancer instance [%s] has stopped", l.instance.Name)
 	return nil
 }
