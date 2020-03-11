c9ecf9891a9674735de1a3090cc8b64f35744cbe
Dan Finneran <daniel.finneran@gmail.com>
2020-03-11 15:45:50 +0000

    Tidy of TCP connectivity

--- pkg/loadbalancer/lb_tcp.go
+++ pkg/loadbalancer/lb_tcp.go
@@ -16,7 +16,6 @@ func (lb *LBInstance) startTCP(bindAddress string) error {
 	fullAddress := fmt.Sprintf("%s:%d", bindAddress, lb.instance.Port)
 	log.Infof("Starting TCP Load Balancer for service [%s]", fullAddress)
 
-	//	l, err := net.Listen("tcp", fmt.Sprintf("%s:%d", bindAddress, lb.instance.Port))
 	laddr, err := net.ResolveTCPAddr("tcp", fullAddress)
 	if nil != err {
 		log.Errorln(err)
@@ -25,7 +24,6 @@ func (lb *LBInstance) startTCP(bindAddress string) error {
 	if nil != err {
 		return fmt.Errorf("Unable to bind [%s]", err.Error())
 	}
-	// a:= net.ListenTCP()
 	go func() {
 		for {
 			select {
@@ -37,13 +35,9 @@ func (lb *LBInstance) startTCP(bindAddress string) error {
 				err = l.Close()
 				if err != nil {
 					return
-					//log.Errorf("Error closing listener [%s]", err)
 				}
 				// Close the stopped channel as the listener has been stopped
-				//lb.mux.Lock()
 				close(lb.stopped)
-				//lb.mux.Unlock()
-				//return
 			default:
 
 				err = l.SetDeadline(time.Now().Add(200 * time.Millisecond))
@@ -60,9 +54,60 @@ func (lb *LBInstance) startTCP(bindAddress string) error {
 					}
 				}
 				go persistentConnection(fd, lb.instance)
-				//go processRequests(lb.instance, fd)
 			}
 		}
+	}()
+	log.Infof("Load Balancer [%s] started", lb.instance.Name)
+
+	return nil
+}
+
+// startTCPDNU - Start TCP service Do not use
+// This stops the service by closing the listener and then ignorning the error from Accept()
+func (lb *LBInstance) startTCPDNU(bindAddress string) error {
+	fullAddress := fmt.Sprintf("%s:%d", bindAddress, lb.instance.Port)
+	log.Infof("Starting TCP Load Balancer for service [%s]", fullAddress)
+
+	//l, err := net.Listen("tcp", fmt.Sprintf("%s:%d", bindAddress, lb.instance.Port))
+	laddr, err := net.ResolveTCPAddr("tcp", fullAddress)
+	if nil != err {
+		log.Errorln(err)
+	}
+	l, err := net.ListenTCP("tcp", laddr)
+	if nil != err {
+		return fmt.Errorf("Unable to bind [%s]", err.Error())
+	}
+	go func() {
+		<-lb.stop
+		log.Debugln("Closing listener")
+
+		// We've closed the stop channel
+		err = l.Close()
+		if err != nil {
+			return
+		}
+		// Close the stopped channel as the listener has been stopped
+		close(lb.stopped)
+
+	}()
+
+	go func() {
+		for {
+
+			fd, err := l.Accept()
+			if err != nil {
+				// Check it it's an accept timeout
+				if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+					continue
+				} else if err != io.EOF {
+					log.Errorf("TCP Accept error [%s]", err)
+					return
+				}
+			}
+			go persistentConnection(fd, lb.instance)
+			//go processRequests(lb.instance, fd)
+		}
+		//	}
 	}()
 	log.Infof("Load Balancer [%s] started", lb.instance.Name)
 
