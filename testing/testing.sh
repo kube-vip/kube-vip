627ec8da3c23696bb5c98cee798b4b3dbe0d5183
thebsdbox <dan@thebsdbox.co.uk>
2020-12-24 17:28:55 +0000

    final fix

--- testing/testing.sh
+++ testing/testing.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-alias kube-vip="docker run --network host --rm plndr/kube-vip:action"
-
-kube-vip manifest pod --interface eth0 --vip 192.168.0.1 --controlplane --arp --services --leaderElection
-kube-vip manifest pod --interface eth0 --vip 192.168.0.1 --controlplane --bgp
-kube-vip manifest pod --interface eth0 --vip 192.168.0.1 --controlplane --bgp --bgppeers 192.168.0.2:12345::true,192.168.0.3:12345::true
-kube-vip manifest pod --interface eth0 --vip 192.168.0.1 --controlplane --arp --leaderElection
+docker run --network host --rm plndr/kube-vip:action manifest pod --interface eth0 --vip 192.168.0.1 --controlplane --arp --services --leaderElection
+docker run --network host --rm plndr/kube-vip:action manifest pod --interface eth0 --vip 192.168.0.1 --controlplane --bgp
+docker run --network host --rm plndr/kube-vip:action manifest pod --interface eth0 --vip 192.168.0.1 --controlplane --bgp --bgppeers 192.168.0.2:12345::true,192.168.0.3:12345::true
+docker run --network host --rm plndr/kube-vip:action manifest pod --interface eth0 --vip 192.168.0.1 --controlplane --arp --leaderElection
