82db862a1a7e4693bd68dc01ca7015ad83b42dc9
thebsdbox <dan@thebsdbox.co.uk>
2021-11-22 20:00:05 +0000

    IPVS fixes

--- pkg/loadbalancer/ipvs.go
+++ pkg/loadbalancer/ipvs.go
@@ -53,21 +53,6 @@ func NewIPVSLB(address string, port int) (*IPVSLoadBalancer, error) {
 		Address:   ipvs.NewIP(net.ParseIP(address)),
 		Scheduler: ROUNDROBIN,
 	}
-	err = c.CreateService(svc)
-	// If we've an error it could be that the IPVS lb instance has been left from a previous leadership
-	if err != nil && strings.Contains(err.Error(), "file exists") {
-		log.Warnf("load balancer for API server already exists, attempting to remove and re-create")
-		err = c.RemoveService(svc)
-		if err != nil {
-			return nil, fmt.Errorf("error re-creating IPVS service: %v", err)
-		}
-		err = c.CreateService(svc)
-		if err != nil {
-			return nil, fmt.Errorf("error re-creating IPVS service: %v", err)
-		}
-	} else if err != nil {
-		return nil, fmt.Errorf("error creating IPVS service: %v", err)
-	}
 
 	lb := &IPVSLoadBalancer{
 		Port:                port,
@@ -88,6 +73,32 @@ func (lb *IPVSLoadBalancer) RemoveIPVSLB() error {
 }
 
 func (lb *IPVSLoadBalancer) AddBackend(address string, port int) error {
+
+	// Check if this is the first backend
+	backends, err := lb.client.Destinations(lb.loadBalancerService)
+	if err != nil && strings.Contains(err.Error(), "file does not exist") {
+		log.Errorf("Error querying backends %s", err)
+	}
+	// If this is our first backend, then we can create the load-balancer service and add a backend
+	if len(backends) == 0 {
+		err = lb.client.CreateService(lb.loadBalancerService)
+		// If we've an error it could be that the IPVS lb instance has been left from a previous leadership
+		if err != nil && strings.Contains(err.Error(), "file exists") {
+			log.Warnf("load balancer for API server already exists, attempting to remove and re-create")
+			err = lb.client.RemoveService(lb.loadBalancerService)
+			if err != nil {
+				return fmt.Errorf("error re-creating IPVS service: %v", err)
+			}
+			err = lb.client.CreateService(lb.loadBalancerService)
+			if err != nil {
+				return fmt.Errorf("error re-creating IPVS service: %v", err)
+			}
+		} else if err != nil {
+			return fmt.Errorf("error creating IPVS service: %v", err)
+		}
+		log.Infof("Created Load-Balancer services on [%s:%d]", lb.loadBalancerService.Address.Net(ipvs.INET).String(), lb.Port)
+	}
+
 	dst := ipvs.Destination{
 		Address:   ipvs.NewIP(net.ParseIP(address)),
 		Port:      uint16(port),
@@ -96,12 +107,15 @@ func (lb *IPVSLoadBalancer) AddBackend(address string, port int) error {
 		FwdMethod: ipvs.Local,
 	}
 
-	err := lb.client.CreateDestination(lb.loadBalancerService, dst)
+	err = lb.client.CreateDestination(lb.loadBalancerService, dst)
 	// Swallow error of existing back end, the node watcher may attempt to apply
 	// the same back end multiple times
 	if err != nil && !strings.Contains(err.Error(), "file exists") {
 		return fmt.Errorf("error creating backend: %v", err)
+	} else {
+		log.Infof("Added backend for [%s:%d] on [%s:%d]", lb.loadBalancerService.Address.Net(ipvs.INET).String(), lb.Port, address, port)
 	}
+
 	return nil
 }
 
