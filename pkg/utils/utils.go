ff25c77c6f20f0c281974f13f20a11140619585c
Marcel Fest <marcel.fest@telekom.de>
2025-04-09 14:35:32 +0200

    Simplify validation check and remove unused code
    Signed-off-by: Marcel Fest <marcel.fest@telekom.de>

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strconv"
 )
 
 func FileExists(filename string) bool {
@@ -18,22 +17,11 @@ func FileExists(filename string) bool {
 // FormatIPWithSubnetMask takes a raw IP address and a subnet mask, and returns a formatted string in CIDR notation.
 func FormatIPWithSubnetMask(rawIP string, subnetMask string) (string, error) {
 
-	// Check if the input is empty
-	if rawIP == "" {
-		return "", fmt.Errorf("no IP address provided")
-	}
-	if subnetMask == "" {
-		return "", fmt.Errorf("no subnetMask provided")
-	}
-	ip := net.ParseIP(rawIP)
-	if ip == nil {
-		return "", fmt.Errorf("invalid IP address: %s", rawIP)
-	}
-
-	if value, err := strconv.Atoi(subnetMask); err != nil || value < 0 || value > 128 {
-		return "", fmt.Errorf("invalid subnet mask: %s", subnetMask)
-	}
-
+	addr := fmt.Sprintf("%s/%s", rawIP, subnetMask)
 	// Check if the input is valid
-	return fmt.Sprintf("%s/%s", ip.String(), subnetMask), nil
+	_, _, err := net.ParseCIDR(addr)
+	if err != nil {
+		return "", fmt.Errorf("invalid CIDR: %q, %w", addr, err)
+	}
+	return addr, nil
 }
