1ad2be3f0b88699bb05bb524283fdd5540a6a6f8
Matt Carpenter <mattcarp88@gmail.com>
2026-04-26 06:32:51 -0600

    feat: add support for BGP unnumbered peers (#1512)
    * feat: add support for BGP unnumbered peers
    
    Signed-off-by: Matthew Carpenter <mattcarp88@gmail.com>
    
    * add test case
    
    Signed-off-by: Matthew Carpenter <mattcarp88@gmail.com>
    
    * rebase to main
    
    Signed-off-by: Matthew Carpenter <mattcarp88@gmail.com>
    
    * fix merge conflict
    
    Signed-off-by: Matthew Carpenter <mattcarp88@gmail.com>
    
    ---------
    
    Signed-off-by: Matthew Carpenter <mattcarp88@gmail.com>

--- pkg/kubevip/config_bgp_test.go
+++ pkg/kubevip/config_bgp_test.go
@@ -1,10 +1,8 @@
-package bgp
+package kubevip
 
 import (
 	"reflect"
 	"testing"
-
-	"github.com/kube-vip/kube-vip/pkg/kubevip"
 )
 
 func TestParseBGPPeerConfig(t *testing.T) {
@@ -14,13 +12,13 @@ func TestParseBGPPeerConfig(t *testing.T) {
 	tests := []struct {
 		name         string
 		args         args
-		wantBgpPeers []kubevip.BGPPeer
+		wantBgpPeers []BGPPeer
 		wantErr      bool
 	}{
 		{
 			name: "IPv4, default port",
 			args: args{config: "192.168.0.10:65000::false,192.168.0.11:65000::false"},
-			wantBgpPeers: []kubevip.BGPPeer{
+			wantBgpPeers: []BGPPeer{
 				{Address: "192.168.0.10", Port: 179, AS: 65000, MultiHop: false},
 				{Address: "192.168.0.11", Port: 179, AS: 65000, MultiHop: false},
 			},
@@ -28,7 +26,7 @@ func TestParseBGPPeerConfig(t *testing.T) {
 		{
 			name: "IPv4, different port",
 			args: args{config: "192.168.0.10:65000::false:180,192.168.0.11:65000::false:190"},
-			wantBgpPeers: []kubevip.BGPPeer{
+			wantBgpPeers: []BGPPeer{
 				{Address: "192.168.0.10", Port: 180, AS: 65000, MultiHop: false},
 				{Address: "192.168.0.11", Port: 190, AS: 65000, MultiHop: false},
 			},
@@ -36,14 +34,22 @@ func TestParseBGPPeerConfig(t *testing.T) {
 		{
 			name: "IPv6, multi-protocol",
 			args: args{config: "[fd00:1111:2222:3333:c7d9:7235:6bf7:5d52]:65501::false/mpbgp_nexthop=auto_sourceif"},
-			wantBgpPeers: []kubevip.BGPPeer{
+			wantBgpPeers: []BGPPeer{
 				{Address: "fd00:1111:2222:3333:c7d9:7235:6bf7:5d52", Port: 179, AS: 65501, MultiHop: false, MpbgpNexthop: "auto_sourceif"},
+			},
+		},
+		{
+			name: "Unnumbered",
+			args: args{config: "unnumbered:eth0,unnumbered:eth1:65000::true/mpbgp_nexthop=auto_sourceif"},
+			wantBgpPeers: []BGPPeer{
+				{Interface: "eth0", MultiHop: false},
+				{Interface: "eth1", AS: 65000, MultiHop: true, MpbgpNexthop: "auto_sourceif"},
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotBgpPeers, err := kubevip.ParseBGPPeerConfig(tt.args.config)
+			gotBgpPeers, err := ParseBGPPeerConfig(tt.args.config)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ParseBGPPeerConfig() error = %v, wantErr %v", err, tt.wantErr)
 				return
